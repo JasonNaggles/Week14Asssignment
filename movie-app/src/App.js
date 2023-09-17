c83ebd272dc50754d01a0fd1f173e8916c7c9556
JasonNaggles <JasonNaggles.verizon.net>
2023-09-17 07:07:28 -0400

    first commit

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Movie from './components/Movie';
 import './components/MoviePosters.css';
+import Star from './components/Star';
 function App() {
+  const [rating, setRating] = useState(3); // Initial rating
+
+  const handleRatingChange = (newRating) => {
+    setRating(newRating);
+
+  };
+
   return (
     <div>
       <Movie />
+      <Star rating={rating} onChange={handleRatingChange} />
+      <p>Selected Rating: {rating}</p>
     </div>
   );
 }
