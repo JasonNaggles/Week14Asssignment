116bc46c10ebfd7c855a3ba76382ef6e4604baa5
JasonNaggles <JasonNaggles.verizon.net>
2023-09-17 06:35:15 -0400

    first commit

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Movie from './components/Movie';
+import './components/MoviePosters.css';
 function App() {
   return (
     <div>
