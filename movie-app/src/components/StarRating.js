f1fd4f0af3143657a40542f03fac74f59605787d
JasonNaggles <JasonNaggles.verizon.net>
2023-09-19 17:43:16 -0400

    first commit

--- movie-app/src/components/StarRating.js
+++ movie-app/src/components/StarRating.js
@@ -1,5 +1,12 @@
 import React from 'react';
+import { FaStar } from 'react-icons/fa';
+
+const StarRating = () => {
+    return (
+    <div>FaStar</div>
+    )
+};
 
 
     
-export default Star;
+export default StarRating;
